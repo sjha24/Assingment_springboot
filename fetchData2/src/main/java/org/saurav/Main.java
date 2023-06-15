6b8bd3c23c7aa5866871cdcd50019c53f733c445
Saurav <saurav24021998@gmail.com>
2023-06-15 11:13:26 +0530

    fetch data

---
+++ fetchData2/src/main/java/org/saurav/Main.java
@@ -0,0 +1,38 @@
+package org.saurav;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws IOException {
+       String urlString = "https://api.zippopotam.us/us/33162";
+        URL url = new URL(urlString);
+
+        //connection
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        int responseCode = connection.getResponseCode();
+
+        //extract information from the connection object
+        if(responseCode == 200){
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder apiData = new StringBuilder();
+            String readLine = null;
+            while((readLine = in.readLine()) != null){
+                apiData.append(readLine);
+            }
+            in.close();
+            JSONObject jsonAPI_response = new JSONObject(apiData.toString());
+            System.out.println(jsonAPI_response.toString());
+        }else{
+            System.out.println("API Call Could not be placed");
+        }
+    }
+}
